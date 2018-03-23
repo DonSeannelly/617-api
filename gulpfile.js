d12adb15207eca39b6c5b37b05a27e258ccc1508
Sean Donnelly <sdonnelly3000@gmail.com>
2018-03-23 17:04:53 -0400

    Revert "Add Azure startup command"
    This reverts commit 0ccde48b218162bb1c501e2fbab02407bbcba1aa.

--- gulpfile.js
+++ gulpfile.js
@@ -37,8 +37,6 @@ gulp.task('start', ['watch', 'assets'], function () {
 
 gulp.task('run:datastore', shell.task('npm run json:server'));
 
-gulp.task('run:azure', shell.task('npm start'));
-
 gulp.task("typedoc", function() {
     return gulp
         .src(["src/*.ts"])
