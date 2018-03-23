d436fea4f0d8591ddab0b2a2abe2b1514f6b7429
Sean Donnelly <sdonnelly3000@gmail.com>
2018-03-23 17:20:25 -0400

    Remove dotenv config

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,7 @@ var graphqlHTTP = require('express-graphql');
 
 import { SchemaBuilder } from "./schema/schema";
 import { JsonStore } from './drivers/json-store';
-import * as dotenv from 'dotenv';
 
-dotenv.config();
 const isProd = process.env.NODE_ENV == 'production' ? true : false;
 
 var app = express();
