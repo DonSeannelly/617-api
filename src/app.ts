d99509cc72e7d0762bd7f01bdbaa03dcd7f3498d
Nick Winner <design@winnerdigital.net>
2018-03-29 18:09:21 -0400

    Implement cors

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,15 @@ var graphqlHTTP = require('express-graphql');
 
 import { SchemaBuilder } from "./schema/schema";
 import { JsonStore } from './drivers/json-store';
+import * as cors from 'cors';
 
 const isProd = process.env.NODE_ENV == 'production' ? true : false;
 
 var app = express();
 
 const dataStore = new JsonStore();
+
+app.use(cors());
 
 app.use('/', graphqlHTTP({
   schema: SchemaBuilder.buildSchema(dataStore),
