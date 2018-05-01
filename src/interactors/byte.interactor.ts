549ffd46c9f6702649af34f751d7d12f1ce0dde8
Sean Donnelly <sdonnelly3000@gmail.com>
2018-05-01 18:38:13 -0400

    Fix faulty function call

--- src/interactors/byte.interactor.ts
+++ src/interactors/byte.interactor.ts
@@ -3,7 +3,7 @@ import { getUser } from "./user.interactor";
 
 export async function getByte(dataStore: DataStore, id: string) {
   const byte = await dataStore.getByte(id);
-  const user = await getUser(dataStore, byte.creatorId);
+  const user = await getUser({ dataStore, id: byte.creatorId });
   delete byte.creatorId;
   return { ...byte, creator: user };
 }
