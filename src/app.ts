540733485a278e10bf965163f932800ccea68547
Sean Donnelly <sdonnelly3000@gmail.com>
2018-05-01 19:30:57 -0400

    Move graphql api from /  to /api

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ var graphqlHTTP = require('express-graphql');
 import { SchemaBuilder } from "./schema/schema";
 import { JsonStore } from './drivers/json-store';
 import * as cors from 'cors';
-import { Response } from "express";
+import { Response, Request } from "express";
 
 const isProd = process.env.NODE_ENV == 'production' ? true : false;
 
@@ -14,14 +14,16 @@ const dataStore = new JsonStore();
 
 app.use(cors());
 
-app.use('/', graphqlHTTP({
+app.use('/api', graphqlHTTP({
   schema: SchemaBuilder.buildSchema(dataStore),
   graphiql: !isProd,
 }));
 
-app.post('/auth0/registerUser', (req, res: Response) => {
+app.post('/auth0/registerUser', (req: Request, res: Response) => {
   console.log(req.body.json());
   res.sendStatus(200);
 })
+
+app.get('/', (req, res) => res.send('Welcome to the 617 API'));
 
 app.listen(process.env.PORT || 4000, () => console.log('API running! prod = ', isProd));
