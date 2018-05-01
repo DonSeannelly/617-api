322af104d131c15c365da434f0bb746523349111
Sean Donnelly <sdonnelly3000@gmail.com>
2018-05-01 19:14:45 -0400

    Add auth0 hook route

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ var graphqlHTTP = require('express-graphql');
 import { SchemaBuilder } from "./schema/schema";
 import { JsonStore } from './drivers/json-store';
 import * as cors from 'cors';
+import { Response } from "express";
 
 const isProd = process.env.NODE_ENV == 'production' ? true : false;
 
@@ -17,5 +18,10 @@ app.use('/', graphqlHTTP({
   schema: SchemaBuilder.buildSchema(dataStore),
   graphiql: !isProd,
 }));
+
+app.post('/auth0/registerUser', (req, res: Response) => {
+  console.log(req.body.json());
+  res.sendStatus(200);
+})
 
 app.listen(process.env.PORT || 4000, () => console.log('API running! prod = ', isProd));
