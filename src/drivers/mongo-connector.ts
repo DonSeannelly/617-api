7c63cf9e62faa8d8af9108c4b4c2a712f77c2990
Sean Donnelly <sdonnelly3000@gmail.com>
2018-05-03 08:56:46 -0400

    Fix unresolved merge conflicts

--- src/drivers/mongo-connector.ts
+++ src/drivers/mongo-connector.ts
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 import { DataStore } from "../interfaces/DataStore";
 import { MongoClient, Db, ObjectID } from "mongodb";
 import * as dotenv from 'dotenv';
-=======
-import { DataStore } from '../interfaces/DataStore';
-import { MongoClient, Db } from 'mongodb';
->>>>>>> bfa9e37328a903bfc1d34d8c56b1cb3fec3cf621
 
 export class MongoConnector implements DataStore {
   private db: Db;
@@ -19,11 +14,7 @@ export class MongoConnector implements DataStore {
 
   async connect(dbURI: string): Promise<void> {
     try {
-<<<<<<< HEAD
       this.db = (await MongoClient.connect(dbURI)).db("software-bytes");
-=======
-      this.db = (await MongoClient.connect(dbURI)).db('test-db');
->>>>>>> bfa9e37328a903bfc1d34d8c56b1cb3fec3cf621
       return Promise.resolve();
     } catch (e) {
       console.log(e);
@@ -65,14 +56,9 @@ export class MongoConnector implements DataStore {
   }
   async createTable(name: string, hostId: string): Promise<string> {
     try {
-<<<<<<< HEAD
       const doc = { _id: (new ObjectID).toString(), name, hostId }
       const result = await this.db.collection("tables").insertOne(doc);
       
-=======
-      const doc = { name, hostId }
-      await this.db.collection('tables').insertOne(doc);
->>>>>>> bfa9e37328a903bfc1d34d8c56b1cb3fec3cf621
       // TODO: Check for success before resolve
       const id = result.ops[0]['_id'];
       return Promise.resolve(id);
@@ -80,7 +66,6 @@ export class MongoConnector implements DataStore {
       return Promise.reject(e);
     }
   }
-<<<<<<< HEAD
   async inviteUserToTable(tableId: string, email: string): Promise<void> {
     try {
       const result = await this.db.collection('tables')
@@ -88,10 +73,6 @@ export class MongoConnector implements DataStore {
     } catch(e) {
       return Promise.reject(e);
     }
-=======
-  inviteUserToTable(tableId: string, email: string): Promise<void> {
-    throw new Error('Method not implemented.');
->>>>>>> bfa9e37328a903bfc1d34d8c56b1cb3fec3cf621
   }
   joinTable(tableId: string, userId: string) {
     throw new Error('Method not implemented.');
